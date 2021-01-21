37e1a3466b7c0e13a2817a1c502805f2471ec7d3
Khulegu <khulegu2002.b@gmail.com>
2021-01-21 16:50:36 +0800

    ui layout change

--- models/screen1.py
+++ models/screen1.py
@@ -3,14 +3,11 @@ from odoo import models, fields, api
 class screen1(models.Model):
     _name = 'screen1'
     _description = 'screen1'
-    ID = fields.Integer(compute="random_ID")
+
     department = fields.Char()
-    work = fields.Char()
+    work = fields.Text()
     date_begin = fields.Datetime(default=fields.Datetime.now())
     date_end = fields.Datetime(default=fields.Datetime.now())
     assigned_to = fields.Char()
     supervisor = fields.Char()
     verified_by = fields.Char()
-
-    def random_ID(self):
-        self.ID = 1
--- models/screen2.py
+++ models/screen2.py
@@ -6,14 +6,10 @@ class performanceState(models.Model):
     _name = 'screen2'
     _description = 'screen2'
     
-    ID = fields.Integer(compute="random_ID")
     from_who = fields.Char()
     from_where = fields.Char()
-    works = fields.Char()
+    works = fields.Text()
     date_begin = fields.Datetime(default=fields.Datetime.now())
     date_end = fields.Datetime(default=fields.Datetime.now())
     assigned_to = fields.Char()
     accepted_by = fields.Char()
-
-    def random_ID(self):
-        self.ID = 1
